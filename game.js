957d5c1fce7d73914d6681b26e1f89cfe9939d81
SobuHasy <sobu.hasy@hotmail.com>
2024-07-24 16:44:34 +0200

    Already added the monkey and made the zookeeper to move

--- game.js
+++ game.js
@@ -4,9 +4,25 @@ document.body.appendChild(app.view);
 
 const zookeeper = PIXI.Sprite.from('assets/zookeeper.png');
 zookeeper.x = 350;
-zookeeper.y = 500;
-
-zookeeper.scale.x = 0.05;
-zookeeper.scale.y = 0.05;
-
+zookeeper.y = 440;
+zookeeper.scale.x = 0.1;
+zookeeper.scale.y = 0.1;
 app.stage.addChild(zookeeper);
+
+gameInterval(function(){
+    const monkey = PIXI.Sprite.from('assets/monkey.png');
+    monkey.x = random(0, 1000);
+    monkey.y = -25;
+    monkey.scale.x = 0.08;
+    monkey.scale.y = 0.08;
+    app.stage.addChild(monkey);
+    flyDown(monkey, 1);
+}, 1000);
+
+function leftKeyPressed(){
+    zookeeper.x = zookeeper.x - 5;
+}
+
+function rightKeyPressed(){
+    zookeeper.x = zookeeper.x + 5;
+}
