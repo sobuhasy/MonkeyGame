710851df734454a9f39f36c36ee3c924d0b96822
SobuHasy <sobu.hasy@hotmail.com>
2024-07-24 16:30:23 +0200

    Adjusted the size of the character sprites

--- game.js
+++ game.js
@@ -3,9 +3,10 @@ const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
 const zookeeper = PIXI.Sprite.from('assets/zookeeper.png');
+zookeeper.x = 350;
+zookeeper.y = 500;
 
 zookeeper.scale.x = 0.05;
 zookeeper.scale.y = 0.05;
-zookeeper.scale.set(500.0);
 
 app.stage.addChild(zookeeper);
