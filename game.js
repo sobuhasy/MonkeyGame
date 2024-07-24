cbf018fc8d2789f1c875d3abbc8c5765cf9ea713
SobuHasy <sobu.hasy@hotmail.com>
2024-07-24 21:56:23 +0200

    The game is almost finished, but the collisions are not recognized!

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const app = new PIXI.Application();
-
+const monkeyList = [];
 document.body.appendChild(app.view);
 
 const zookeeper = PIXI.Sprite.from('assets/zookeeper.png');
@@ -16,6 +16,7 @@ gameInterval(function(){
     monkey.scale.x = 0.08;
     monkey.scale.y = 0.08;
     app.stage.addChild(monkey);
+    monkeyList.push(monkey);
     flyDown(monkey, 1);
 
     waitForCollision(zookeeper, monkey).then(function() {
@@ -34,9 +35,16 @@ function rightKeyPressed(){
 
 function spaceKeyPressed(){
     const banana = PIXI.Sprite.from('assets/banana.png');
-    banana.x = zookeeper.x;
-    banana.y = zookeeper.y;
+    banana.x = zookeeper.x + 13;
+    banana.y = 500;
     banana.scale.x = 0.06;
     banana.scale.y = 0.06;
+    flyUp(banana, 1);
     app.stage.addChild(banana);
+
+    waitForCollision(banana, monkeyList).then(function([banana, monkey]) {
+        app.stage.removeChild(banana);
+        app.stage.removeChild(monkey);
+        stopGame();
+    })
 }
